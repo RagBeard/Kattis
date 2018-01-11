67ae3ff36bf61da89332b3e1afc521b16da7e903
Daniel Johansson <danijoh@gmail.com>
2018-01-11 17:12:32 +0100

    trivial carrots solution

---
+++ Trivial/carrots/carrots.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kattis.IO;
+
+namespace Kattis.Trivial.carrots
+{
+	class carrots
+	{
+		static void Main(string[] args)
+		{
+			Scanner scan = new Scanner();
+
+			scan.NextInt();
+
+			Console.Write(scan.NextInt());
+		}
+	}
+}
