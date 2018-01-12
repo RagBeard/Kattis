47e7271b43c3018bf9221eea4793a9fec16472bc
Daniel Johansson <danijoh@gmail.com>
2018-01-12 21:51:53 +0100

    completed easy zanzibar problem

---
+++ Easy/zanzibar/zanzibar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kattis.IO;
+
+namespace Kattis.Easy.zanzibar
+{
+	class zanzibar
+	{
+		static void Main(string[] args)
+		{
+			Scanner scan = new Scanner();
+			
+			int tests = scan.NextInt();
+
+			for (int i = 0; i < tests; ++i)
+			{
+				int year = scan.NextInt();
+				int notBorn = 0;
+
+				while(year > 0)
+				{
+					int prevYear = year;
+					year = scan.NextInt();
+
+					int expectedMax = prevYear * 2;
+					int diff = year - expectedMax;
+
+					if ( diff > 0 )
+					{
+						// there are more turtles than could have been born here
+						notBorn += diff;
+					}
+				}
+
+				Console.WriteLine(notBorn);
+				
+			}
+
+			
+		}
+	}
+}
