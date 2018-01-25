9416bb4694f4531f4f792d78533371ec65a55faf
Daniel Johansson <danijoh@gmail.com>
2018-01-25 15:11:58 +0100

    trivial twostones complete

---
+++ Trivial/twostones/twostones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kattis.IO;
+
+namespace Kattis.Trivial.twostones
+{
+	class TwoStones
+	{
+		static void Main(string[] args)
+		{
+			Scanner scan = new Scanner();
+
+
+			int n = scan.NextInt();
+
+			int half = n / 2;
+
+			if ( (half * 2) == n)
+			{
+				Console.WriteLine("Bob");
+			}
+			else
+			{
+				Console.WriteLine("Alice");
+			}			
+		}
+	}
+}
